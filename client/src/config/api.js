985303acc37a91f914d4c305db3cdf563c605f9d
ZarathosDeath <nando_rhapsody@hotmail.com>
2021-05-04 23:30:56 -0300

    add yarn start script

---
+++ client/src/config/api.js
@@ -0,0 +1,12 @@
+
+import 'dotenv/config';
+import axios from 'axios';
+
+const prodUrl = 'https://trabalho-gces-api.herokuapp.com';
+const devUrl = "http://localhost:8000";
+
+const url = process.env.NODE_ENV === 'development' ? devUrl : prodUrl;
+
+export const api = axios.create({
+  baseURL: url
+})
