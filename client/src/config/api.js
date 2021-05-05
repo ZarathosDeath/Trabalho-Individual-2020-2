8722025a5282197dc2ebc9ef815fc0dcb1a83975
ZarathosDeath <nando_rhapsody@hotmail.com>
2021-05-04 23:48:14 -0300

    fix api production url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,7 +2,7 @@
 import 'dotenv/config';
 import axios from 'axios';
 
-const prodUrl = 'https://trabalho-gces-api.herokuapp.com';
+const prodUrl = 'https://api-egcs.herokuapp.com/';
 const devUrl = "http://localhost:8000";
 
 const url = process.env.NODE_ENV === 'development' ? devUrl : prodUrl;
